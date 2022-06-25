49439aa6318769b29d0c21035d4baf77127821c8
Cooper Saye <coopersaye@gmail.com>
2022-06-25 00:10:57 -0700

    Add banner to header.

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+
 import styles from '../styles/components/Header.module.css';
 
 export default function Header() {
   return (
     <div>
+    <div className={styles.container}>
+      <div className={styles.bannerWrap}>
+        <Image
+          src="/img/banner.svg" width="280" height="80" alt="banner"
+          className={styles.banner}
+        />
+      </div>
     </div>
   );
 }
