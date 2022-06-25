09770b547571398595952ee961492871552ea343
Cooper Saye <coopersaye@gmail.com>
2022-06-25 11:08:26 -0700

    Update footer text.

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/components/Footer.module.css';
 export default function Footer() {
   return (
     <div className={styles.container}>
-      <p>© Copyright <b>Joseph C.</b> All rights reserved</p>
+      <p>Captain Coder LLC © 2016 - 2022</p>
     </div>
   );
 }
