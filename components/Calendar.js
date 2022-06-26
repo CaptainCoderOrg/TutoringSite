19e09c3afd445a8cdd842550fe9a32783e44aa72
Cooper Saye <coopersaye@gmail.com>
2022-06-26 00:36:40 -0700

    Replace calendar with calendly widget.

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,4 +1,5 @@
 import MenuBookIcon from '@mui/icons-material/MenuBook';
+import { InlineWidget } from "react-calendly";
 
 import styles from '../styles/components/Calendar.module.css';
 
@@ -9,20 +10,12 @@ export default function Calendar() {
         <MenuBookIcon fontSize="large" />
         <a
           className="url"
-          href="https://jcollard.io/book"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={() => document.getElementsByClassName('calendly-badge-widget')[0].click()}
         >
           Book a session
         </a>
       </p>
-      <iframe
-        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&mode=WEEK&src=am9zZXBobWNvbGxhcmRAZ21haWwuY29t&src=OGYyZzltbGR1djV2NzAxcnJucGxmZDluMzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=bGlicDlqbTZhcjUzNmpvbTlydWV1ZWxhbW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F6BF26&color=%23AD1457&color=%233F51B5"
-        style={{ border: 'solid 1px #777' }}
-        height="600"
-        frameBorder="0"
-        scrolling="no"
-      />
+      <InlineWidget url="https://calendly.com/d/dm2-845-v4s" />
     </div>
   );
 }
