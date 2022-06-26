c42cc90dadd12a6d67f3b116fc2ad09c497d89c2
Cooper Saye <coopersaye@gmail.com>
2022-06-26 00:56:32 -0700

    Update contact section layout.

--- components/Contact.js
+++ components/Contact.js
@@ -1,3 +1,5 @@
+import TextField from '@mui/material/TextField';
+
 import { useState } from 'react';
 
 import styles from '../styles/components/Contact.module.css';
@@ -7,25 +9,60 @@ export default function Contact() {
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
 
+  function inputColor(color) {
+    return {
+      '& label.Mui-focused': { color: color },
+      '& .MuiInput-underline:after': { borderBottomColor: color },
+      '& .MuiOutlinedInput-root': {
+        '&.Mui-focused fieldset': { borderColor: color },
+      }
+    };
+  }
+
   return (
     <div className={styles.container}>
       <h1>Contact</h1>
-      <form onSubmit={() => {}}>
-        <input
+      <form onSubmit={e => {
+        e.preventDefault();
+        window.alert(JSON.stringify({ name, email, message }));
+      }}>
+        <TextField
+          sx={{
+            marginBottom: '16px',
+            ...inputColor('#2196f3')
+          }}
+          label="Name"
           value={name}
           onChange={e => setName(e.target.value)}
-          required
+          autoComplete="off" spellCheck="false"
+          fullWidth required
         />
-        <input
+        <TextField
+          sx={{
+            marginBottom: '16px',
+            ...inputColor('#2196f3')
+          }}
           type="email"
+          label="Email"
           value={email}
           onChange={e => setEmail(e.target.value)}
+          autoComplete="off" spellCheck="false"
+          fullWidth required
         />
-        <textarea
+        <TextField
+          sx={{
+            marginBottom: '16px',
+            ...inputColor('#2196f3')
+          }}
+          label="Message"
+          placeholder="Add your message here..."
           value={message}
           onChange={e => setMessage(e.target.value)}
+          autoComplete="off" spellCheck="false"
+          fullWidth required
+          multiline rows={4}
         />
-        <button>
+        <button className="blueButton">
           Submit Message
         </button>
       </form>
