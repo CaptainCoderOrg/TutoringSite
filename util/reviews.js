ff6c97642ccadab15d891bc31eadaa426a2183e4
Cooper Saye <coopersaye@gmail.com>
2022-06-26 00:44:11 -0700

    Extract reviews.

---
+++ util/reviews.js
@@ -0,0 +1,32 @@
+export const reviews = [
+  {
+    title: "Just amazing!",
+    content: "Joe is a natural teacher and a phenomenal programmer. Very patient, positive, and a good listener. He will break things down into small digestible pieces so you understand the logic and concepts more clearly. I highly recommend him.",
+    author: "Kurt"
+  },
+  {
+    title: "Very competent and knowledgable tutor",
+    content: "Joseph C. has helped guide the way for my java data structures course. He is an extremely knowledgable and patient tutor, who was able to walk me through any concepts that I did not understand. He has a very clear and precise way of teaching, and made it extremely easy to absorb more complex topics. Joseph is responsive and available, I would 1000% recommend Joseph C. for anyone looking to improve their cs skills.",
+    author: "Anthony"
+  },
+  {
+    title: "Skilled at Teaching and Breaking Down Complex Topics",
+    content: "Joseph is a skilled and professional teacher committed to making sure that you understand the content he is explaining to you. He helped me during a college Computer Architecture course by breaking down the concepts into manageable chunks of digestible information that led me to not only Acing the class but understanding what I was learning within the class.",
+    author: "Ryan"
+  },
+  {
+    title: "Great tutor",
+    content: "Joseph did an amazing job walking me through our lesson and explaining different topics when necessary. He went at a great pace, and explained this very clearly and concisely",
+    author: "Max"
+  },
+  {
+    title: "I couldn't have chosen a better tutor",
+    content: "I have had the pleasure of working with Joseph on two separate projects. The subjects were Linux command line and GCP Big query. Joseph was kind and very knowledgeable about both. What really struck me about our sessions was his patience and ability to tailor his teaching style to my understanding. I highly recommend him as a tutor. If you are hesitant about making a choice on which tutor to hire, I would encourage you to look no further.",
+    author: "Panthea"
+  },
+  {
+    title: "Knowledgeable and Patient",
+    content: "Had a great session with Joseph. Was very helpful, quick, and informative! What more can you ask for?? We were able to complete my assignment without issues. Joseph was so kind as to go through the entire thought process with me.",
+    author: "Caitlin"
+  }
+];
