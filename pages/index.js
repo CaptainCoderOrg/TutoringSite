ffca6fe09bff3f5194fccc59d10cb8cb03b542f7
Cooper Saye <coopersaye@gmail.com>
2022-06-26 00:51:10 -0700

    Implement section scrolling.

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,22 @@ import Calendar from '../components/Calendar';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 
+import { useRef } from 'react';
+
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
+  const testimonialsRef = useRef();
+  const calendarRef = useRef();
+  const contactRef = useRef();
+
+  function scrollTo(section) {
+    if (section === 'about') window.scrollTo(0, 0);
+    else if (section === 'testimonials') testimonialsRef.current.scrollIntoView();
+    else if (section === 'calendar') calendarRef.current.scrollIntoView();
+    else if (section === 'contact') contactRef.current.scrollIntoView();
+  }
+
   return (
     <div className={styles.container}>
       <Header scrollTo={scrollTo} />
