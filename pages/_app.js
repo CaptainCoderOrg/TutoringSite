9336a793dfa3c1997089ee57c6c0b2307a7d1205
Cooper Saye <coopersaye@gmail.com>
2022-06-26 00:36:13 -0700

    Create calendly popup widget.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import Head from 'next/head';
+import { PopupWidget } from "react-calendly";
+
+import { useEffect, useState } from 'react';
 
 import '../styles/globals.css';
 
 export default function App(props) {
   const { Component, pageProps } = props;
+
+  const [root, setRoot] = useState(undefined);
+
+  // get root element on start
+  useEffect(() => {
+    setRoot(document.getElementById('__next'));
+  }, []);
 
   return (
     <>
@@ -13,6 +23,13 @@ export default function App(props) {
         <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
       </Head>
+      <PopupWidget
+        url="https://calendly.com/d/dm2-845-v4s"
+        rootElement={root}
+        text="Schedule a Lesson"
+        textColor="#ffffff"
+        color="#2196f3"
+      />
       <Component {...pageProps} />
     </>
   );
