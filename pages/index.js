3911c7312aa2bddbf7655ca906cc4e0407a20a01
Cooper Saye <coopersaye@gmail.com>
2022-06-26 00:51:02 -0700

    Set up index page.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
+import Header from '../components/Header';
+import About from '../components/About';
+import Testimonials from '../components/Testimonials';
+import Calendar from '../components/Calendar';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
   return (
-    <div>
+    <div className={styles.container}>
+      <Header scrollTo={scrollTo} />
+      <About scrollTo={scrollTo} />
+      <span className={styles.bookmark} ref={testimonialsRef} />
+      <Testimonials />
+      <span className={styles.bookmark} ref={calendarRef} />
+      <Calendar />
+      <span className={styles.bookmark} ref={contactRef} />
+      <Contact />
+      <Footer />
     </div>
   );
 }
