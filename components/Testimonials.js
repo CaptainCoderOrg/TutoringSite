f56bbfb9e890ba062b212597f3a28b08eae0e4bc
Cooper Saye <coopersaye@gmail.com>
2022-06-26 00:49:30 -0700

    Update testimonials layout.

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -2,26 +2,12 @@ import styles from '../styles/components/Testimonials.module.css';
 
 export default function Testimonials() {
   return (
-    <div>
-      <div>
-        <p>Just amazing!</p>
-        <p>Joe is a natural teacher and a phenomenal programmer. Very patient, positive, and a good listener. He will break things down into small digestible pieces so you understand the logic and concepts more clearly. I highly recommend him.</p>
-        <p>Kurt, 21 lessons with Joseph</p>
+    <div className={styles.container}>
+      <div className={styles.reviewsWide}>
+        <ReviewCarousel centerMode={true} />
       </div>
-      <div>
-        <p>Very competent and knowledgable tutor</p>
-        <p>Joseph C. has helped guide the way for my java data structures course. He is an extremely knowledgable and patient tutor, who was able to walk me through any concepts that I did not understand. He has a very clear and precise way of teaching, and made it extremely easy to absorb more complex topics. Joseph is responsive and available, I would 1000% recommend Joseph C. for anyone looking to improve their cs skills.</p>
-        <p>Anthony, 6 lessons with Joseph</p>
-      </div>
-      <div>
-        <p>Skilled at Teaching and Breaking Down Complex Topics</p>
-        <p>Joseph is a skilled and professional teacher committed to making sure that you understand the content he is explaining to you. He helped me during a college Computer Architecture course by breaking down the concepts into manageable chunks of digestible information that led me to not only Acing the class but understanding what I was learning within the class.</p>
-        <p>Ryan, 7 lessons with Joseph</p>
-      </div>
-      <div>
-        <p>I couldn&quot;t have chosen a better tutor</p>
-        <p>I have had the pleasure of working with Joseph on two separate projects. The subjects were Linux command line and GCP Big query. Joseph was kind and very knowledgeable about both. What really struck me about our sessions was his patience and ability to tailor his teaching style to my understanding. I highly recommend him as a tutor. If you are hesitant about making a choice on which tutor to hire, I would encourage you to look no further.</p>
-        <p>Panthea, 4 lessons with Joseph</p>
+      <div className={styles.reviews}>
+        <ReviewCarousel centerMode={false} />
       </div>
     </div>
   );
