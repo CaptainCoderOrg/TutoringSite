b0400fa16026b94720c5b19af0bf430b859fabaa
Cooper Saye <coopersaye@gmail.com>
2022-06-27 00:44:05 -0700

    Create Welcome component.

---
+++ components/Welcome.js
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+
+import styles from '../styles/components/Welcome.module.css';
+
+export default function Welcome(props) {
+  const { scrollTo } = props;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <h1>Captain Coder&apos;s Academy</h1>
+        <p>Become a coding hero with one-on-one and group lessons at Captain Coder&apos;s Academy!</p>
+        <button
+          className="blueButton"
+          onClick={() => scrollTo('about')}
+        >
+          Meet the Captain!
+        </button>
+      </div>
+      <div className={styles.splash}>
+        <Image
+          className={styles.logo}
+          src="/img/logo-clean.svg"
+          width="512"
+          height="512"
+          alt="logo"
+        />
+      </div>
+    </div>
+  );
+}
