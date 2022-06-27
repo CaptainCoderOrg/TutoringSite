4afad119662d589083236b69dd2174ed5bcaaca9
Joseph Collard <jcollard@users.noreply.github.com>
2022-06-27 13:09:59 -0700

    chore: Trim a few testimonials to reduce height.

--- util/reviews.js
+++ util/reviews.js
@@ -6,12 +6,12 @@ export const reviews = [
   },
   {
     title: "Very competent and knowledgable tutor",
-    content: "Joseph C. has helped guide the way for my java data structures course. He is an extremely knowledgable and patient tutor, who was able to walk me through any concepts that I did not understand. He has a very clear and precise way of teaching, and made it extremely easy to absorb more complex topics. Joseph is responsive and available, I would 1000% recommend Joseph C. for anyone looking to improve their cs skills.",
+    content: "Joseph has helped guide the way for my java data structures course. He is an extremely knowledgable and patient tutor, who was able to walk me through any concepts that I did not understand.",
     author: "Anthony"
   },
   {
     title: "Skilled at Teaching and Breaking Down Complex Topics",
-    content: "Joseph is a skilled and professional teacher committed to making sure that you understand the content he is explaining to you. He helped me during a college Computer Architecture course by breaking down the concepts into manageable chunks of digestible information that led me to not only Acing the class but understanding what I was learning within the class.",
+    content: "Joseph helped me during a college Computer Architecture course by breaking down the concepts into manageable chunks of digestible information that led me to not only Acing the class but understanding what I was learning within the class.",
     author: "Ryan"
   },
   {
@@ -21,7 +21,7 @@ export const reviews = [
   },
   {
     title: "I couldn't have chosen a better tutor",
-    content: "I have had the pleasure of working with Joseph on two separate projects. The subjects were Linux command line and GCP Big query. Joseph was kind and very knowledgeable about both. What really struck me about our sessions was his patience and ability to tailor his teaching style to my understanding. I highly recommend him as a tutor. If you are hesitant about making a choice on which tutor to hire, I would encourage you to look no further.",
+    content: "I have had the pleasure of working with Joseph on two separate projects. The subjects were Linux command line and GCP Big query. What really struck me about our sessions was his patience and ability to tailor his teaching style to my understanding. I highly recommend him as a tutor.",
     author: "Panthea"
   },
   {
