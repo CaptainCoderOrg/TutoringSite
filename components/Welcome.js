594707e5aa81a37ffe9a660c6e046b9009045f9f
Cooper Saye <coopersaye@gmail.com>
2022-06-27 19:35:55 -0700

    Condense welcome section layout.

--- components/Welcome.js
+++ components/Welcome.js
@@ -5,24 +5,26 @@ export default function Welcome(props) {
 
   return (
     <div className={styles.container}>
-      <div className={styles.info}>
-        <h1>Captain Coder&apos;s Academy</h1>
-        <p>Become a coding hero with one-on-one and group lessons at Captain Coder&apos;s Academy!</p>
-        <button
-          className="blueButton"
-          onClick={() => scrollTo('about')}
-        >
-          Meet the Captain!
-        </button>
-      </div>
-      <div className={styles.splash}>
-        <img
-          className={styles.logo}
-          src="/img/logo-clean.svg"
-          width="512"
-          height="512"
-          alt="logo"
-        />
+      <div className={styles.content}>
+        <div className={styles.info}>
+          <h1>Captain Coder&apos;s Academy</h1>
+          <p>Become a coding hero with one-on-one and group lessons at Captain Coder&apos;s Academy!</p>
+          <button
+            className="blueButton"
+            onClick={() => scrollTo('about')}
+          >
+            Meet the Captain!
+          </button>
+        </div>
+        <div className={styles.splash}>
+          <img
+            className={styles.logo}
+            src="/img/logo-clean.svg"
+            width="300"
+            height="300"
+            alt="logo"
+          />
+        </div>
       </div>
     </div>
   );
