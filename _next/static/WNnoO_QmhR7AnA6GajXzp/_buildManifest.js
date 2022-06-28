d25a2fb666f39d597e911440eabd6013d612a36a
csaye <coopersaye@gmail.com>
2022-06-28 18:20:23 +0000

    Deploying to gh-pages from  @ d4ecc437861d41708555d0773047d60f69e92f2c 🚀

---
+++ _next/static/WNnoO_QmhR7AnA6GajXzp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/87-f6133810f34ee7f0.js","static/css/1953b590761c2263.css","static/chunks/pages/index-0df6b35e31577fdb.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/nuRlPZzzXJizbOlWKsSeU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/468-88d8ae42751111fa.js","static/css/09503db0d8ea1818.css","static/chunks/pages/index-f6a329aadd346dd8.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
