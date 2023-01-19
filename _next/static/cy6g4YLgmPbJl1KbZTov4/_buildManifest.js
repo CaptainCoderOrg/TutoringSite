bfd9488ca1827d3889bc4abb88f4f6b0de4be192
jcollard <josephmcollard@gmail.com>
2023-01-19 16:29:56 +0000

    Deploying to gh-pages from  @ 6b8cbcb87db106ea291b83172b41343af4e79de5 🚀

--- _next/static/cy6g4YLgmPbJl1KbZTov4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/180-9bf07d2eeb8d81b8.js","static/css/520d0961d70d4316.css","static/chunks/pages/index-5eac8b4f75f916b2.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
